66081fdd799d519e0811f63e512de51aa1ff21a3
Saswata7 <113622852+Saswata7@users.noreply.github.com>
2022-12-17 18:01:33 +0530

    Update 5_📊_Statistics_101.py

--- pages/5_📊_Statistics_101.py
+++ pages/5_📊_Statistics_101.py
@@ -6,21 +6,29 @@ display_logo()
 
 st.header('CURATED | 📊 STAT 101')
 st.markdown(""" 
-Statistics is fundamentally very important if you wnat to study Machine Learning. There are multiple sources from which you can study Stat.\n
-We shall provide some sources here.\n            
+Statistics is fundamentally very important if you want to study Machine Learning properly.   
+
+*Quick tip - StatQuest by Josh Starmer is awesome! https://www.youtube.com/@statquest*
+
+Here is a playlist to give you a general overview of statistical concepts.
 """,unsafe_allow_html=True)
 
-st.write("You can follow the youtube channel StatQuest by Josh Starmer.")
-embed_yt_video('https://www.youtube.com/@statquest')
+yt_content_stored('stat_fundamentals')
 
-st.markdown("""Probability is important too. You can refer Sheldon M. Ross: A First Course in Probability\n
-            Also there is a fantastic book on Probability and Statistics by Professor Arnab Chakraborty(ISI Kolkata) with the same name\n
-            Other than that Khan Academy also provides great resources for Probablity and Statistics.
-            """,unsafe_allow_html=True)
-embed_yt_video('https://www.youtube.com/watch?v=uhxtUt_-GyM&list=PL1328115D3D8A2566')
-embed_yt_video('https://www.youtube.com/watch?v=uzkc-qNVoOk&list=PLC58778F28211FA19')
+st.markdown("""
+___
+Probability is important too. You can refer Sheldon M. Ross: A First Course in Probability
 
-st.write("Random Variable and Probability Distributions")
-embed_yt_video('https://www.youtube.com/watch?v=3v9w79NhsfI&list=PLU5aQXLWR3_xDN0M2ZeZ_zHIia0e42_3O')
+Also there is a fantastic book on Probability and Statistics by Professor Arnab Chakraborty(ISI Kolkata) with the same name
+
+Other than that Khan Academy also provides great resources for Probablity and Statistics.""",unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    embed_yt_video('https://www.youtube.com/watch?v=uhxtUt_-GyM&list=PL1328115D3D8A2566')
+with col2:
+    embed_yt_video('https://youtu.be/uzkc-qNVoOk')
+with col3:
+    embed_yt_video('https://www.youtube.com/watch?v=3v9w79NhsfI&list=PLU5aQXLWR3_xDN0M2ZeZ_zHIia0e42_3O')
 
 footer()
